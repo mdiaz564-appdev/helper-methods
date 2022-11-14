ba4c1719e3811dd21d000eda5b2a53e2f8402abb
mdiaz564 <37diazm@gmail.com>
2022-11-14 20:52:03 +0000

    gave names to routes and started using them

--- config/routes.rb
+++ config/routes.rb
@@ -2,30 +2,30 @@ Rails.application.routes.draw do
   #get "/", controller:"movies", action:"index" 
   #get "/" => "movies#index"
   root "movies#index"
-  
+
   # Routes for the Movie resource:
 
   # CREATE
   #post("/movies", { :controller => "movies", :action => "create" })
-  post "/movies" => "movies#create"
+  post "/movies" => "movies#create", as: :movies #movies_url and movies_path
 
-  #get("/movies/new", { :controller => "movies", :action => "new" })
-  get "/movies/new" => "movies#new"
+ 
+  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path
 
   # READ
-  #get("/movies", { :controller => "movies", :action => "index" })
+
   get "/movies" => "movies#index"
-  #get("/movies/:id", { :controller => "movies", :action => "show" })
-  get "/movies/:id" => "movies#show"
+
+  get "/movies/:id" => "movies#show", as: :movie #movie_path() (expects and argument to populate :id segment)
 
   # UPDATE
-  #patch("/movies/:id", { :controller => "movies", :action => "update" })
+
   patch "/movies/:id" => "movies#update"
-  #get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
-  get "/movies/:id/edit" => "movies#edit"
+
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie #edit_movie_path() (expects and argument to populate :id segment)
 
   # DELETE
-  #delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+
   delete "/movies/:id" => "movies#destroy"
   #------------------------------
 end
